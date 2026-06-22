2b11d2cbe247a43387fb02921c706a4fc98470ba
Amar3339 <amarg3339@gmail.com>
2026-06-22 22:27:06 +0530

    in this i practice Data Conversion

---
+++ basics/Data_conversion.js
@@ -0,0 +1,26 @@
+var name="amar"
+const n=123
+let score=true
+let scored=1
+
+console.log(typeof name);
+data_type=Number(name)
+let s=Number(score)
+let boolean_convert=Boolean(scored)
+let a=String(123)
+console.log(data_type);
+console.log(a);
+console.log(s);
+console.log(boolean_convert);
+
+
+
+// numbers easily convert
+// string not converted in number give value nan
+// true-->1 ,fals-->0
+// 1 --> true ,0-->false
+
+
+
+
+
