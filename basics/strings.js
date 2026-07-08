89308bfdff300d09668c1aa4f1de6dc3a150b41c
Amar3339 <amarg3339@gmail.com>
2026-07-08 11:06:02 +0530

    Practice JavaScript string methods

---
+++ basics/strings.js
@@ -0,0 +1,22 @@
+const name="Amarmd"
+const repo=30
+console.log(`hello my name is ${name} and my repo count is ${repo}` )
+
+const newname=new String('amarmohammad')
+console.log(newname);
+console.log(newname[0])
+console.log(newname.length);
+console.log(newname.blink())
+console.log(newname.toUpperCase());
+const user= "  superman    "
+console.log(user.trim())
+console.log(newname.endsWith('d'));
+console.log(newname.includes('d '));
+console.log(newname.substring(0,4));
+const user1="shaktiman"
+console.log(user1.replace('shaktiman','batman'))
+console.log(newname.split('m'))
+
+
+
+
